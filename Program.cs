af7c83e686b36a9ca940ec6e9b38cc58faeec666
Andreas Lauter <be-ndee@users.noreply.github.com>
2024-03-10 11:26:32 +0100

    init grpc client

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using grpc_client_todo;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("http://localhost:5181");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
