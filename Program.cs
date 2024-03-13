b6f9d860c5e4203f7b4e9cd378f1e38629557bdc
Andreas Lauter <be-ndee@users.noreply.github.com>
2024-03-13 20:19:23 +0100

    add proto and call todo items service

--- Program.cs
+++ Program.cs
@@ -4,8 +4,12 @@ using grpc_client_todo;
 
 // The port number must match the port of the gRPC server.
 using var channel = GrpcChannel.ForAddress("http://localhost:5181");
-var client = new Greeter.GreeterClient(channel);
-var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
-Console.WriteLine("Greeting: " + reply.Message);
-Console.WriteLine("Press any key to exit...");
+var client = new TodoItems.TodoItemsClient(channel);
+
+var allReply = await client.GetListAsync(new ListRequest { OnlyOpen = false });
+Console.WriteLine("Items: " + allReply.Items);
+
+var onlyOpenReply = await client.GetListAsync(new ListRequest { OnlyOpen = true });
+Console.WriteLine("Items: " + onlyOpenReply.Items);
+
 Console.ReadKey();
